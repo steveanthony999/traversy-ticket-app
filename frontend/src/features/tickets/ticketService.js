6d11b83a0711646be08055a178e7a169d9c67cd2
Steven Woodward <steveanthony999@gmail.com>
2022-03-16 01:35:31 -0700

    set up ticket functionality and back button

---
+++ frontend/src/features/tickets/ticketService.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const API_URL = '/api/tickets/';
+
+// Create new ticket
+const createTicket = async (ticketData, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const res = await axios.post(API_URL, ticketData, config);
+
+  return res.data;
+};
+
+const ticketService = { createTicket };
+
+export default ticketService;
